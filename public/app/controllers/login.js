b623b3b797bf77f414591b9d2622719d70fcb25a
Thomas DE BONA <thomas.debona@gmail.com>
2014-06-24 23:53:18 +0200

    Simulate the login

--- public/app/controllers/login.js
+++ public/app/controllers/login.js
@@ -1,7 +1,14 @@
 'use strict';
 
-angular.module('homework').controller('LoginController', ['$scope',
-    function($scope) {
-      $scope.hello = 'Hello Login';
+angular.module('homework').controller('LoginController', ['$scope', '$state',
+    function($scope, $state) {
+      $scope.user = {};
+
+      $scope.tryLogin = function tryLogin() {
+        var hasError = $scope.loginForm.login.$error.email;
+        if (!hasError && $scope.user.login && $scope.user.password) {
+          $state.go('activity')
+        }
+      };
     }
 ]);
