cd038193a568f46353c87a06d485c4f821ded192
Thomas DE BONA <thomas.debona@gmail.com>
2014-06-25 12:41:29 +0200

    Create fake log data

--- public/app/controllers/activity.js
+++ public/app/controllers/activity.js
@@ -2,6 +2,79 @@
 
 angular.module('homework').controller('ActivityController', ['$scope',
     function($scope) {
-      $scope.hello = 'Hello Activity';
+      $scope.recentActivities = [
+        {
+          date:    new Date('25 Jun 2014 10:30:00'),
+          browser: 'chrome',
+          ip:      '81.64.177.27',
+          location: {
+            latitude:  43.7031,
+            longitude: 7.2661
+          }
+        },
+        {
+          date:    new Date('24 Jun 2014 10:30:00'),
+          browser: 'firefox',
+          ip:      '81.64.177.27',
+          location: {
+            latitude:  43.7031,
+            longitude: 7.2661
+          }
+        },
+        {
+          date:    new Date('25 Jun 2014 12:30:00'),
+          browser: 'safari',
+          ip:      '36.65.65.65',
+          location: {
+            latitude:  40.7031,
+            longitude: 8.2661
+          }
+        },
+        {
+          date:    new Date('15 Jun 2014 10:30:00'),
+          browser: 'opera',
+          ip:      '99.99.99.99',
+          location: {
+            latitude:  3.4031,
+            longitude: 7.3421
+          }
+        },
+        {
+          date:    new Date('25 Jun 2014 10:30:00'),
+          browser: 'chrome',
+          ip:      '81.64.177.27',
+          location: {
+            latitude:  43.7031,
+            longitude: 7.2661
+          }
+        },
+        {
+          date:    new Date('24 Jun 2014 10:30:00'),
+          browser: 'firefox',
+          ip:      '81.64.177.27',
+          location: {
+            latitude:  43.7031,
+            longitude: 7.2661
+          }
+        },
+        {
+          date:    new Date('25 Jun 2014 12:30:00'),
+          browser: 'safari',
+          ip:      '36.65.65.65',
+          location: {
+            latitude:  40.7031,
+            longitude: 8.2661
+          }
+        },
+        {
+          date:    new Date('15 Jun 2014 10:30:00'),
+          browser: 'opera',
+          ip:      '99.99.99.99',
+          location: {
+            latitude:  3.4031,
+            longitude: 7.3421
+          }
+        }
+      ];
     }
 ]);
