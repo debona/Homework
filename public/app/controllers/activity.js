ceb34be4b6e99082ceb312120e2b182aaa3f9645
Thomas DE BONA <thomas.debona@gmail.com>
2014-06-26 20:30:18 +0200

    Refactor activity controller to use the auth user data

--- public/app/controllers/activity.js
+++ public/app/controllers/activity.js
@@ -1,31 +1,8 @@
 'use strict';
 
-angular.module('homework').controller('ActivityController', ['$scope',
-	function($scope) {
-		$scope.randomizeActivity = function randomizeActivity(){
-			var today = new Date();
-			var browsers = ['chrome', 'opera', 'firefox', 'safari'];
-
-			return {
-				date:    new Date(today.getTime() - _.random(0, 60*60*24*7*1000)),
-				browser: browsers[_.random(0, browsers.length -1)],
-				ip:      _.random(12, 250) + '.' + _.random(12, 250) + '.' + _.random(12, 250) + '.' +_.random(12, 250),
-				location: {
-					latitude:  48.8589437 + Math.random() * 0.08 - 0.04,
-					longitude: 2.3452575 + Math.random() * 0.16 - 0.08
-				}
-			};
-		};
-
-		$scope.randomize = function randomize(length) {
-			var activities = [];
-			for (var i = 0; i < length; i++) {
-				activities.push($scope.randomizeActivity());
-			}
-			return activities;
-		}
-
-		$scope.recentActivities = $scope.randomize(10).sort( function (item, nextItem) {
+angular.module('homework').controller('ActivityController', ['$scope', 'auth',
+	function($scope, auth) {
+		$scope.recentActivities = auth.user.recentActivities.sort( function (item, nextItem) {
 			return item.date < nextItem.date;
 		});
 
