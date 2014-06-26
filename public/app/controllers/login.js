2a7eb11d1f4c7f7365312a37a30998b0b424f6bc
Thomas DE BONA <thomas.debona@gmail.com>
2014-06-26 17:20:37 +0200

    Display notifications on login

--- public/app/controllers/login.js
+++ public/app/controllers/login.js
@@ -1,13 +1,21 @@
 'use strict';
 
-angular.module('homework').controller('LoginController', ['$scope', '$state',
-	function($scope, $state) {
+angular.module('homework').controller('LoginController', ['$scope', '$state', 'notificationCenter',
+	function($scope, $state, notificationCenter) {
 		$scope.user = {};
+
+		$scope.tryOAuthLogin = function tryOAuthLogin() {
+			notificationCenter.display('Bienvenue John Doe. (connecté avec : OAuth)', 'success');
+			$state.go('activity');
+		};
 
 		$scope.tryLogin = function tryLogin() {
 			var hasError = $scope.loginForm.login.$error.email;
 			if (!hasError && $scope.user.login && $scope.user.password) {
-				$state.go('activity')
+				notificationCenter.display('Bienvenue John Doe. (connecté avec : ' + $scope.user.login + ')', 'success');
+				$state.go('activity');
+			} else {
+				notificationCenter.display('Connexion impossible avec ces identifiants', 'warning');
 			}
 		};
 	}
