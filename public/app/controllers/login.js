92ca1bac2d0b9146ad255164f1e28151c76fac13
Thomas DE BONA <thomas.debona@gmail.com>
2014-06-26 20:29:34 +0200

    Use the auth service in the login controller

--- public/app/controllers/login.js
+++ public/app/controllers/login.js
@@ -1,18 +1,28 @@
 'use strict';
 
-angular.module('homework').controller('LoginController', ['$scope', '$state', 'notificationCenter',
-	function($scope, $state, notificationCenter) {
+angular.module('homework').controller('LoginController', ['$scope', '$state', 'auth', 'notificationCenter',
+	function($scope, $state, auth, notificationCenter) {
 		$scope.user = {};
 
 		$scope.tryOAuthLogin = function tryOAuthLogin() {
-			notificationCenter.display('Bienvenue John Doe. (connecté avec : OAuth)', 'success');
-			$state.go('activity');
+			auth.login('oauth', 'password');
+
+			if (auth.isAuthenticated()) {
+				notificationCenter.display('Bienvenue ' + auth.user.name + '. (connecté avec : OAuth)', 'success');
+				$state.go('activity');
+			} else {
+				notificationCenter.display('Connexion impossible avec ces identifiants', 'warning');
+			}
 		};
 
 		$scope.tryLogin = function tryLogin() {
 			var hasError = $scope.loginForm.login.$error.email;
 			if (!hasError && $scope.user.login && $scope.user.password) {
-				notificationCenter.display('Bienvenue John Doe. (connecté avec : ' + $scope.user.login + ')', 'success');
+				auth.login($scope.user.login, $scope.user.password);
+			}
+
+			if (auth.isAuthenticated()) {
+				notificationCenter.display('Bienvenue ' + auth.user.name + '. (connecté avec : ' + auth.user.email + ')', 'success');
 				$state.go('activity');
 			} else {
 				notificationCenter.display('Connexion impossible avec ces identifiants', 'warning');
