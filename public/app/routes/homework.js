eaadbcfce47cf59f51cd982854cdc1446aad5837
Thomas DE BONA <thomas.debona@gmail.com>
2014-06-27 00:52:28 +0200

    Activity page require login

--- public/app/routes/homework.js
+++ public/app/routes/homework.js
@@ -3,6 +3,19 @@
 angular.module('homework').config(['$stateProvider', '$urlRouterProvider',
 	function($stateProvider, $urlRouterProvider) {
 
+		var requireAuth = function requireAuth($q, auth, notificationCenter) {
+			var deferred = $q.defer();
+
+			if (auth.isAuthenticated()) {
+				deferred.resolve();
+			} else {
+				deferred.reject();
+				notificationCenter.display('Vous devez être connecté pour accéder à cette page.', 'warning');
+			}
+
+			return deferred.promise;
+		};
+
 		$urlRouterProvider.otherwise('/');
 
 		$stateProvider.state('home', {
@@ -10,7 +23,8 @@ angular.module('homework').config(['$stateProvider', '$urlRouterProvider',
 			templateUrl: '/app/views/index.html'
 		}).state('activity', {
 			url: '/activity',
-			templateUrl: '/app/views/activity.html'
+			templateUrl: '/app/views/activity.html',
+			resolve: { requireAuth: requireAuth }
 		}).state('login', {
 			url: '/login',
 			templateUrl: '/app/views/login.html'
